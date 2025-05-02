c97d3a825d3fb5243fa6b96377af2b1e2adf1449
e40125 <ethan40125bard@gmail.com>
2025-05-02 17:49:16 +0800

    added api key & test connectivity

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,5 +1,9 @@
+# experiments/utils.py
+
 import pickle
 import os
+from dotenv import load_dotenv
+import random
 
 def load_prepared_dataset():
     """
@@ -21,10 +25,30 @@ def load_prepared_dataset():
     
 if __name__ == "__main__":
     dataset = load_prepared_dataset()
-# Test Block (remove later)
-    if dataset:
-        print(f"Loaded dataset contains {len(dataset)} subtasks.")
-        first_subtask = list(dataset.keys())[0]
-        print(f"First subtask: {first_subtask}")
-        first_question_en = dataset[first_subtask]['en'][0]
-        print("Example English question:", first_question_en)
+
+
+
+def load_api_keys():
+    """
+    Loads API keys from the .env file.
+    """
+    load_dotenv() # This looks for .env in the current directory and loads variables
+    google_api_key = os.getenv("GOOGLE_API_KEY")
+    mistral_api_key = os.getenv("MISTRAL_API_KEY")
+
+    if not google_api_key:
+        print("Warning: GOOGLE_API_KEY not found in .env")
+    if not mistral_api_key:
+        print("Warning: MISTRAL_API_KEY not found in .env")
+
+    return google_api_key, mistral_api_key
+
+
+
+# # Test Block for load dataset(remove later)
+#     if dataset:
+#         print(f"Loaded dataset contains {len(dataset)} subtasks.")
+#         first_subtask = list(dataset.keys())[0]
+#         print(f"First subtask: {first_subtask}")
+#         first_question_en = dataset[first_subtask]['en'][0]
+#         print("Example English question:", first_question_en)
