3a3dab36687907b43d29eea079b66efe6a2bcd27
盧音孜 <75139223+kkyzl@users.noreply.github.com>
2025-05-02 17:36:43 +0800

    chore(prompts): finish basic prompt template and formatting function (in common.py)

---
+++ prompts/prompt_templates.py
@@ -0,0 +1,62 @@
+# Wording for different language
+
+INTRO = {
+    "EN-US": "Answer the following multiple choice question.",
+    "FR-FR": "Répondez à la question à choix multiples suivante."
+}
+
+INSTRUCTION = {
+    "EN-US": "The last line of your response should be of the following format: 'Answer: $LETTER' (without quotes) where LETTER is one of ABCD. Think step by step before answering.",
+    "FR-FR": "La dernière ligne de votre réponse doit être au format suivant : « Réponse : $LETTRE » (sans les guillemets). Réfléchissez étape par étape avant de répondre."
+}
+
+CHOICE = {
+    "EN-US": "choices",
+    "FR-FR": "choix"
+}
+
+# Template
+
+QUERY_TEMPLATE_PLAIN = """
+{Intro}
+{Instruction}
+
+{Question}
+
+A) {A}
+B) {B}
+C) {C}
+D) {D}
+""".strip()
+
+QUERY_TEMPLATE_XML = """
+<!--
+{Intro}
+{Instruction}
+-->
+
+<question>
+  <text>{Question}</text>
+  <choises>
+    <A>{A}</A>
+    <B>{B}</B>
+    <C>{C}</C>
+    <D>{D}</D>
+  </choises>
+</question>
+""".strip()
+
+QUERY_TEMPLATE_JSON = """
+// {Intro}
+// {Instruction}
+
+{
+  "question": "{Question}",
+  "choises": {
+    "A": "{A}",
+    "B": "{B}",
+    "C": "{C}",
+    "D": "{D}"
+  }
+}
+""".strip()
