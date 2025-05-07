f0bda5aa95991037f8e50d72bc18942bc12fab52
盧音孜 <75139223+kkyzl@users.noreply.github.com>
2025-05-07 20:15:03 +0800

    refactor(experiments/utils): move prompt formatting function to experiments/utils.py

--- experiments/utils.py
+++ experiments/utils.py
@@ -6,6 +6,15 @@ from dotenv import load_dotenv
 from mistralai import Mistral
 from google import genai
 
+from prompts.prompt_templates import (
+    QUERY_TEMPLATE_PLAIN,
+    QUERY_TEMPLATE_JSON,
+    QUERY_TEMPLATE_XML,
+    INTRO,
+    INSTRUCTION,
+    CHOICE
+)
+
 def load_prepared_dataset():
     """
     Loads the prepared dataset from a pickle file.
@@ -53,3 +62,23 @@ def load_api_keys():
 #         print(f"First subtask: {first_subtask}")
 #         first_question_en = dataset[first_subtask]['en'][0]
 #         print("Example English question:", first_question_en)
+
+def format_multichoice_question(row, style='plain', lang="EN-US"):
+    ###################################
+    # Input:                          #
+    # - row: Question, A, B, C, D     #
+    # - style: 'plain', 'json', 'xml' #
+    # - lang: "EN-US", "FR-FR"        #
+    # Output: formatted prompt        #
+    ###################################
+    intro = INTRO[lang]
+    instruction = INSTRUCTION[lang]
+    template = None
+    # choice = CHOICE[lang]
+    if style == 'json':
+        template = QUERY_TEMPLATE_JSON
+    elif style == 'xml':
+        template = QUERY_TEMPLATE_XML
+    else:
+        template = QUERY_TEMPLATE_PLAIN
+    return template.format(Intro = intro, Instruction = instruction, **row) #Choice = choice
