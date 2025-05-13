76dff8f23f103c9a651567878969d1d94fa94811
盧音孜 <75139223+kkyzl@users.noreply.github.com>
2025-05-13 10:05:36 +0800

    feat(experiments): add post-fix program

---
+++ commands/analysis.sh
@@ -0,0 +1,23 @@
+python analysis/analyze_results.py \
+results/fix_exp/gemini-2.0-flash-lite_en_base_20250511-101950_fix.jsonl \
+--output_metrics_file results/analysis/gemini-2.0-flash-lite_en_base_20250511-101950_fix_analysis.json
+
+python analysis/analyze_results.py \
+results/fix_exp/gemini-2.0-flash-lite_en_json_20250511-101947_fix.jsonl \
+--output_metrics_file results/analysis/gemini-2.0-flash-lite_en_json_20250511-101947_fix_analysis.json
+
+python analysis/analyze_results.py \
+results/fix_exp/gemini-2.0-flash-lite_en_xml_20250511-150449_fix.jsonl \
+--output_metrics_file results/analysis/gemini-2.0-flash-lite_en_xml_20250511-150449_fix_analysis.json
+
+python analysis/analyze_results.py \
+results/fix_exp/gemini-2.0-flash-lite_fr_base_20250511-150459_fix.jsonl \
+--output_metrics_file results/analysis/gemini-2.0-flash-lite_fr_base_20250511-150459_fix_analysis.json
+
+python analysis/analyze_results.py \
+results/fix_exp/gemini-2.0-flash-lite_fr_json_20250511-214603_fix.jsonl \
+--output_metrics_file results/analysis/gemini-2.0-flash-lite_fr_json_20250511-214603_fix_analysis.json
+
+python analysis/analyze_results.py \
+results/fix_exp/gemini-2.0-flash-lite_fr_xml_20250511-214536_fix.jsonl \
+--output_metrics_file results/analysis/gemini-2.0-flash-lite_fr_xml_20250511-214536_fix_analysis.json
