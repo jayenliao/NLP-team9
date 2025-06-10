27f8111896de0ba30d171add21f4b800f4349af4
盧音孜 <75139223+kkyzl@users.noreply.github.com>
2025-06-10 15:35:44 +0800

    docs(analysis_new): doc for analysis_new

--- analysis_new/plot.py
+++ analysis_new/plot.py
@@ -1,6 +1,18 @@
 
 from analyze_grouping import Analyze
 
+'''
+targets:
+[(A,B,C), ...]
+Where 
+A: array of main target specs, which will be distinguished by colors
+B: array of specs that split the targets, which will be distiniguished by shapes
+C: array of tuples that filter the data: ({spec}, {value})
+ex:
+(["subtask"], ["input_format", "output_format"], [("language", "fr"), ("model", "mistral-small-latest")])
+means It will draw plots for the set of data that language be French and used Mistral model. The data will
+will be split into subtasks and input_format + output_format. 
+'''
 targets = [
     # (["subtask"], ["language"], [("model", "gemini-2.0-flash")]),
     # (["subtask"], ["language"], [("model", "mistral-small-latest")]), 
@@ -28,14 +40,25 @@ fr_min = 0
 fr_max = 0.45
 fr2_min = 0
 fr2_max = 0.1
+rsd_min = 0
+rsd_max = 0.13
 
+'''
+plot_specs:
+[(A, B, C, D, E, F, G), ...]
+A: column for x's value
+B: column for y's value
+C: title of the plot
+D, E: min, max of x's points
+F, G: min, max of y's points
+'''
 plot_specs = [
     ("accuracy", "avg_word_count", "accuracy vs avg_word_count", acc_min, acc_max, awc_min, awc_max),
     ("accuracy", "FR", "accuracy vs Fluctuation Rate", acc_min, acc_max, fr_min, fr_max),
     ("accuracy", "FR_2", "accuracy vs Fluctuation Rate 2", acc_min, acc_max, fr2_min, fr2_max),
     ("FR", "avg_word_count", "Fluctuation Rate vs avg_word_count", fr_min, fr_max, awc_min, awc_max),
     ("FR_2", "avg_word_count", "Fluctuation Rate 2 vs avg_word_count", fr2_min, fr2_max, awc_min, awc_max),
-    ("FR", "RSD", "FR vs RSD", fr_min, fr_max, 0, 0.13),
+    ("FR", "RSD", "FR vs RSD", fr_min, fr_max, rsd_min, rsd_max),
     ("FR", "accuracy_A", "FR vs accuracy_A", fr_min, fr_max, acc_min, acc_max),
     ("FR", "accuracy_B", "FR vs accuracy_B", fr_min, fr_max, acc_min, acc_max),
     ("FR", "accuracy_C", "FR vs accuracy_C", fr_min, fr_max, acc_min, acc_max),
